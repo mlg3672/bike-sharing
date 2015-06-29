3406290a6d04366bfc6d856da7e2949411707a07
mlg3672 <michele.goe@gmail.com>
2015-06-29 13:25:38 +0800

    added timeseries split

--- bikeshare.R
+++ bikeshare.R
@@ -11,13 +11,6 @@ setwd("~/Documents/KaggleProjects/bike-sharing")
 train<-read.csv("data/train.csv")
 test<-read.csv("data/test.csv")
 
-#split train into testing, validation, and training
-inBuild<-createDataPartition(y=train$count,p=0.6,list=FALSE)
-validation<-train[inBuild,]
-buildData<-train[-inBuild,]
-inTrain<- createDataPartition(y=buildData$count,p=0.6,list=FALSE)
-training<-buildData[inTrain,]
-testing<-buildData[-inTrain,]
 
 #function capture feature other than count and transform data
 extractFeatures <- function(data) {
@@ -32,22 +25,51 @@ extractFeatures <- function(data) {
                 "hour", 
                 "month", 
                 "year",
-                "day",
+                "weekday",
+                "monthday",
                 "night")
   data$hour <- hour(ymd_hms(data$datetime))
   data$month <-month(ymd_hms(data$datetime))
   data$year <- year(ymd_hms(data$datetime))
-  data$day <- wday(as.Date(data$datetime,label=TRUE))
+  data$monthday <- mday(ymd_hms(data$datetime))
+  data$weekday <- wday(as.Date(data$datetime,label=TRUE))
   data$night <- lapply(data$hour,function (x) as.integer(x>7))
   return(data[,features])
 }
 
-data<-extractFeatures(training)
+#split train into testing, validation, and training
+inBuild<-createDataPartition(y=train$count,p=0.6,list=FALSE)
+validation<-train[inBuild,]
+buildData<-train[-inBuild,]
+inTrain<- createDataPartition(y=buildData$count,p=0.6,list=FALSE)
+training<-buildData[inTrain,]
+testing<-buildData[-inTrain,]
+
+# split data by time 
+## use first 10 days to predict next 7 days
+ts1 <- extractFeatures(train)
+ts1$count <- train$count
+tsTrain<-ts1[ts1$monthday<=10,]
+tsTest<-ts1[ts1$monthday>10,]
+
+## try use time series to view data
+ts1 <- ts(train[,-c(10,11)], frequency = 24)
+ts1Train<-window(ts1,start=1,end=5)
+ts1Test<-window(ts1,start=5,end=(7-0.01))
+plot(ts1Train)
+# moving average
+
+
+# exponential smoothing
+ets1 <- ets(ts1Train,model="MMM")
+fcast <-forecast(ets1)
+plot(fcast,col="black")
+lines(ts1Test,col="red")
 xnames <- colnames(training)[1:11]
-featurePlot(x=training[, xnames], y=training$count, plot="pairs")
+featurePlot(x=training[, xnames], y=training$count, plot="pairs") # warning takes > 5 min
 
 # random forest prediction algorithm
-rf <- randomForest(extractFeatures(data), data$count, ntree=100, importance=TRUE)
+rf <- randomForest(extractFeatures(training), training$count, ntree=100, importance=TRUE)
 imp <- importance(rf, type=1)
 featureImportance <- data.frame(Feature=row.names(imp), Importance=imp[,1])
 
